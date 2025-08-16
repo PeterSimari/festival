e11dd55694c2b3be213372f3d2fba331c45e7144
Peter Simari <44033799+PeterSimari@users.noreply.github.com>
2025-08-16 09:12:41 -0400

    initial commit

---
+++ festival/festival/festivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  festivalApp.swift
+//  festival
+//
+//  Created by Peter Simari on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct festivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ festival/festivalTests/festivalTests.swift
@@ -0,0 +1,17 @@
+//
+//  festivalTests.swift
+//  festivalTests
+//
+//  Created by Peter Simari on 8/16/25.
+//
+
+import Testing
+@testable import festival
+
+struct festivalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
